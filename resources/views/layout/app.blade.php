b83395b3ad4365d897d6f013dbbdd720ffd5d8af
5730367321-PC <j_pcr@hotmail.com>
2017-06-14 17:42:55 +0700

    j

---
+++ resources/views/layout/app.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>@yield('title')</title>
+    <link rel="stylesheet" href="/css/app.css">
+    <link rel="stylesheet" href="{!! asset('css/bootstrap-datepicker.css') !!}">
+    <script type="text/javascript" src="/js/app.js"></script>
+    <script type="text/javascript" src="{!! asset('js/bootstrap-datepicker.js') !!}"></script>
+  </head>
+  <body>
+    @include('include.nav')
+
+    @yield('content')
+  </body>
+</html>
