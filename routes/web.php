82ad902cb4cacf2482fe51f0867a67e6b348c6e0
5730367321-PC <j_pcr@hotmail.com>
2017-06-21 17:14:56 +0700

    j 17.14 21/6/17

--- routes/web.php
+++ routes/web.php
@@ -15,9 +15,6 @@ Route::get('/', function () {
     return view('login');
 });
 
-Route::get('/welcome', function () {
-    return view('welcome');
-});
 
 Route::get('/add_task', function () {
     return view('add_task');
