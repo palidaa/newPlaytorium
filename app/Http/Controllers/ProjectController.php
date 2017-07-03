568113ca3b4e850e320141438fb8b869e3524cc4
Kantanat Chukiat <poom.kie0412@gmail.com>
2017-07-03 10:51:24 +0700

    sort project

--- app/Http/Controllers/ProjectController.php
+++ app/Http/Controllers/ProjectController.php
@@ -17,7 +17,12 @@ class ProjectController extends Controller
 
     public function showProjectList(){
     
-      $users = DB::select('select * from projects');
+      $users = DB::table('projects')
+              ->select(DB::raw("*"))
+              ->orderBy('status','desc')
+              ->orderBy('prj_no','desc')
+              ->get();
+        
       return view('project')->with('projects',$users)
       ->with('num',"")
       ->with('name',"");
@@ -66,6 +71,8 @@ class ProjectController extends Controller
            ->select(DB::raw("*"))
            ->where('prj_no',$no)
            ->where('prj_name', 'like','%' . $name . '%'  )
+           ->orderBy('status','desc')
+           ->orderBy('prj_no','desc')
            ->get();
          }
       else {
@@ -73,6 +80,8 @@ class ProjectController extends Controller
              ->select(DB::raw("*"))
              ->where('prj_no',$no)
              ->orwhere('prj_name', 'like','%' . $name . '%'  )
+             ->orderBy('status','desc')
+             ->orderBy('prj_no','desc')
              ->get();
       }
 
