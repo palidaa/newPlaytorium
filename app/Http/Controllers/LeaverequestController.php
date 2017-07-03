be4e7828472865b02cdf5aec548bafec6022bdf8
MINTRA NANKONGNAEB <mind_lover_soul@hotmail.com>
2017-07-03 18:23:43 +0700

    mail 80%

--- app/Http/Controllers/LeaverequestController.php
+++ app/Http/Controllers/LeaverequestController.php
@@ -29,12 +29,33 @@ class LeaverequestController extends Controller
 
   public function addLeave(Request $request)
   {
-      DB::insert('insert into leaverequest_of_employee values (?,?,?,?,?,?)', [(Auth::id()),$request->input('from'),$request->input('to'),$request->input('leave_type'),'0',$request->input('purpose')]);
+
+      $data = DB::select('SELECT * FROM employees WHERE id = ? ', [Auth::id()] );
+      $user = DB::select('SELECT e.id,e.type,e.department,e.carry_annual_leave,u.email FROM users u join employees e on e.email=u.email where u.id= ?' , [Auth::id()]  );
+      DB::insert('insert into leaverequest_of_employee values (?,?,?,?,?,?)', [$user[0]->id,$request->input('from'),$request->input('to'),$request->input('leave_type'),'0',$request->input('purpose')]);
+      $leave = DB::select('select ifnull(sum(cal_days(l.from,l.to)),0) as leave_used from leaverequest_of_employee l where l.id= ? and leave_type= ? and year(l.from)= year(?)' , [$user[0]->id,$request->input('leave_type'), $request->input('to') ] );
+      $leave_days= DB::select('select cal_days( ? , ? ) as leave_days ' , [$request->input('from'),$request->input('to')]  );
+      $year_leave = 0;
 
       $from = explode('-' ,$request->input('from') );
       $to = explode('-' ,$request->input('to') );
+
       $month_from = "";
       $month_to = "";
+      $leave_type = "";
+
+      switch ($request->input('leave_type')) {
+      case "Annual Leave":
+          $leave_type = "ลาพักร้อน";
+          break;
+      case "Personal Leave":
+          $leave_type = "ลากิจ";
+          break;
+      case "Sick Leave":
+          $leave_type = "ลาป่วย";
+          break;
+      }
+
       switch ($from[1]) {
       case 1:
           $month_from = "มกราคม";
@@ -112,9 +133,31 @@ class LeaverequestController extends Controller
           $month_to = "ธันวาคม";
           break;
       }
+
+      switch ($request->input('leave_type')) {
+        case 'Annual Leave':
+         if($user[0]->type==1){
+            $year_leave=6+$user[0]->carry_annual_leave;
+          }else if($user[0]->type>=2 && $user[0]->type<=7){
+            $year_leave=12+$user[0]->carry_annual_leave;
+          }else if($user[0]->type>=8){
+            $year_leave=15+$user[0]->carry_annual_leave;
+          }
+          break;
+        case 'Personal Leave':
+          $year_leave=6;
+          break;
+        case 'Sick Leave':
+          $year_leave=30;
+          break;
+      }
+
       $mail = array('date_from'=>$from[2],'month_from'=>$month_from, 'year_from'=> ($from[0]+543) ,
-       'leave_type' => $request->input('leave_type') ,'purpose'=> $request->input('purpose'),
-       'date_to'=>$to[2],'month_to'=>$month_to, 'year_to'=> ($to[0]+543) );
+       'leave_type' => $leave_type ,'purpose'=> $request->input('purpose'),
+       'date_to'=>$to[2],'month_to'=>$month_to, 'year_to'=> ($to[0]+543)
+       ,'data' => $data ,'line1'=> $year_leave , 'line2'=>$leave[0]->leave_used, 'line3'=>( $year_leave -$leave[0]->leave_used)
+       ,'leave_day'=>$leave_days[0]->leave_days
+      );
 
       //return view('mail')->with('mail' , $mail);
 
