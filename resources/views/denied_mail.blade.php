c53ebc0666a9a7bc87afbfd17b8178c2f5fab561
palida <palida.th@hotmail.com>
2017-07-11 17:13:58 +0700

    leaveRequestEIEI

---
+++ resources/views/denied_mail.blade.php
@@ -0,0 +1,23 @@
+@extends('layouts.appMail')
+
+@section('title', 'Leave Request Approved Status')
+
+@section('content')
+<div class="container">
+  <div class="row">
+    <!-- show Remaining Leave Day -->
+    <div class="col-md-8 col-md-offset-2">
+      <h2>Leave Request Approved Status</h2>
+      <hr>
+
+      <div class="row">
+        <div class="form-group col-md-5">
+          <label for="Leave_Type">This request has been denied successfully.</label>
+        </div>
+      </div>
+      <!-- 2block -->
+    </div>
+    <!-- container all -->
+  </div>
+</div>
+@endsection
--- resources/views/include/nav.blade.php
+++ resources/views/include/nav.blade.php
@@ -12,20 +12,21 @@
     </div>
     <div class="collapse navbar-collapse" id="myNavbar">
       <ul class="nav navbar-nav">
-        @if(Auth::user()->user_type=="Admin")
-			<li><a href="/admin/holiday">Holiday</a></li>
-			<li><a href="/admin/new_user">New user</a></li>
-		@endif
         <li><a href="/timesheet">Timesheet</a></li>
         <li><a href="/leave_request">Leave Request</a></li>
         <li><a href="/project">Project</a></li>
-		<li><a href="/report">Report</a></li>
+		    <li><a href="/report">Report</a></li>
+
       </ul>
       <ul class="nav navbar-nav navbar-right">
         <li class="dropdown"><a class="dropdown-toggle" data-toggle="dropdown" href="#">
           <span class="glyphicon glyphicon-user"></span> {{ Auth::user()->name }}
           <span class="caret"></span></a>
           <ul class="dropdown-menu">
+            @if(Auth::user()->user_type=="Admin")
+              <li><a href="/admin/holiday">Add Holiday</a></li>
+              <li><a href="/admin/new_user">Add New user</a></li>
+            @endif
             <li><a href="{{ route('logout') }}"
                 onclick="event.preventDefault();
                          document.getElementById('logout-form').submit();">
