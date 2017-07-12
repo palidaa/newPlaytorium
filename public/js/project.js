93a9f5342d9886e927569b4f2e303196883370d2
Noppawit Thairungroj <noppawit.trr@gmail.com>
2017-07-12 10:12:30 +0700

    fix pre-compiled showing

--- public/js/project.js
+++ public/js/project.js
@@ -63,12 +63,12 @@
 /******/ 	__webpack_require__.p = "";
 /******/
 /******/ 	// Load entry module and return exports
-/******/ 	return __webpack_require__(__webpack_require__.s = 158);
+/******/ 	return __webpack_require__(__webpack_require__.s = 151);
 /******/ })
 /************************************************************************/
 /******/ ({
 
-/***/ 128:
+/***/ 121:
 /***/ (function(module, exports) {
 
 new Vue({
@@ -119,10 +119,10 @@ new Vue({
 
 /***/ }),
 
-/***/ 158:
+/***/ 151:
 /***/ (function(module, exports, __webpack_require__) {
 
-module.exports = __webpack_require__(128);
+module.exports = __webpack_require__(121);
 
 
 /***/ })
