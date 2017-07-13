92313f8df7f0a877e27c182749cc8f04d35e1e6e
Sopita Punyakham <amint.dinger@gmail.com>
2017-07-13 13:15:01 +0700

    edit leave history

--- resources/views/leave_request_history.blade.php
+++ resources/views/leave_request_history.blade.php
@@ -7,12 +7,8 @@
 <div class="container">
   <div class="row">
     <div class="col-md-10 col-md-offset-1">
-
-    <div class="row">
-      <div class="col-md-6">
-      <p style="font-size:30px; font-weight:semibold;">Leave request history</p>
-      </div>
-
+    <h2>Leave request history</h2>
+    <br>
           <table class="table table-hover table-striped">
             <tr style="font-size:20px;">
                <th>Leave Type</th>
@@ -25,20 +21,15 @@
              @for ($i = 0;$i<sizeof($leave_request_history);$i++)
               <tr>
                 <td>{{ $leave_request_history[$i]->leave_type}}</td>
-                <td>{{ $leave_request_history[$i]->from }}</td>
-                <td>{{ $leave_request_history[$i]->to }}</td>
+                <td>{{ $leave_request_history[$i]->leave_from }}</td>
+                <td>{{ $leave_request_history[$i]->leave_to }}</td>
                 <td>{{ $leave_request_history[$i]->purpose }}</td>
                 <td style="color:#C4B20F;">{{ $leave_request_history[$i]->status }}</td>
 
               </tr>
              @endfor
-
-
-
-
           </table>
-
-
-      </div>
     </div>
+  </div>
+</div>
 @endsection
