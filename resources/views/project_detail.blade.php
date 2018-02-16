64129b672a6180be5fb5182b49e9b64dd9e67ca8
Noppawit Thairungroj <noppawit.trr@gmail.com>
2018-02-16 16:09:12 +0700

    Added back button to project detail page

--- resources/views/project_detail.blade.php
+++ resources/views/project_detail.blade.php
@@ -3,7 +3,6 @@
 @section('title', 'Project Detail')
 
 @section('content')
-
 <div class="container" >
   <div class="row">
     <div class="col-md-10 col-md-offset-1">
@@ -16,7 +15,10 @@
     @endif
 
     <!-- Project Detail -->
-    <h2>Project Detail</h2>
+    <h2>
+      <a href="/project"><span class="glyphicon glyphicon-triangle-left" aria-hidden="true"></span></a>
+      Project Detail
+    </h2>
     <hr>
     <div class="row">
       <div class="col-md-3">
