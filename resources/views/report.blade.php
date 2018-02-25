3a870deefcd547478c5517499b56ff2a67641f33
5730367321-PC <j_pcr@hotmail.com>
2018-02-25 17:05:15 +0700

    remove scrollbar report

--- resources/views/report.blade.php
+++ resources/views/report.blade.php
@@ -10,9 +10,9 @@
       <div class="col-md-8 col-md-offset-2">
         <h2>Report</h2>
         <hr>
-        <div class="container">
+        <div class="col-md-12">
           <div class="row">
-            <div class="col-md-3">
+            <div class="col-md-4">
               <label>Report type</label>
               <select class="form-control" v-model="type">
                 <option>Timesheet</option>
@@ -23,13 +23,13 @@
             </div>
           </div>
           <div class="row">
-            <div class="col-md-3">
+            <div class="col-md-4">
               <label>Year</label>
               <select class="form-control" v-model="selectedYear">
                 <option v-for="year in years">@{{ year }}</option>
               </select>
             </div>
-            <div class="col-md-3" v-show="type=='Timesheet'">
+            <div class="col-md-4" v-show="type=='Timesheet'">
               <label>Month</label>
               <select class="form-control" v-model="selectedMonth">
                 <option v-for="month in months">@{{ month }}</option>
@@ -37,7 +37,7 @@
             </div>
           </div>
           <div class="row">
-            <div class="col-md-6" v-show="type=='Timesheet'">
+            <div class="col-md-8" v-show="type=='Timesheet'">
               <label>Project</label>
               <select class="form-control" v-model="selectedProject">
                 <option v-for="project in projects">@{{ project.prj_no }} - @{{ project.prj_name }}</option>
