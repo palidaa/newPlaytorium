931bc82341e62e1205b3ca9c678cd69b8201d42c
5730367321-PC <j_pcr@hotmail.com>
2018-02-27 14:32:00 +0700

    addAnnualDays

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -4,6 +4,7 @@ namespace App\Console;
 
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
+use DB;
 
 class Kernel extends ConsoleKernel
 {
@@ -26,6 +27,41 @@ class Kernel extends ConsoleKernel
     {
         // $schedule->command('inspire')
         //          ->hourly();
+
+    
+        $schedule->call(function () {
+            $employees = DB:: select('select * from employees');
+            $annualDay = 0;
+
+            foreach($employees as $employee){
+                if($employee->type == 1){
+                    $annualDay = 6;
+                }
+                else if($employee->type > 1 &&  $employee->type < 8){
+                    $annualDay = 12;
+                }
+                else if($employee->type >= 8 ){
+                    $annualDay = 15;
+                }
+            $leave = DB::select('select SUM(l.totalhours)*0.125 as leave_used 
+            from leaverequest_of_employee l 
+            where l.id= ? and leave_type= ? and year(l.leave_date)= ? and l.status !=?' 
+            , [$employee->id,'Annual Leave', date('Y',strtotime(' -1 year')), 'Rejected' ] );
+
+            $sum_leave = $annualDay  - $leave[0]->leave_used;
+           if($sum_leave>=5) { $sum_leave=5; }
+           else if($sum_leave<=0) { $sum_leave=0; }
+
+            DB::update('update timesheet.employees set carry_annual_leave = ? where timesheet.employees.id =?', [$sum_leave,$employee->id]);
+
+            
+
+
+            }
+           // DB::update('update timesheet.employees set carry_annual_leave = ? where timesheet.employees.id =10002', [rand(1,100)]);
+
+        });
+    
     }
 
     /**
